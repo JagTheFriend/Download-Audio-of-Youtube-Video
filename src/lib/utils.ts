108465c56838c931e152f7b9d42917ae60874058
JagTheFriend <JagTheFriend12@gmail.com>
2024-02-17 21:56:18 +0200

    Created util function
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,28 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import {
+  DataToSend,
+  ResponseReceived,
+  VideoFormat,
+  videoFinderUrl,
+} from "./type";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export async function searchSong(songName: string): Promise<DataToSend[]> {
+  const response = await fetch(videoFinderUrl + songName);
+  const data: ResponseReceived = <ResponseReceived>await response.json();
+  const dataToSend: VideoFormat[] = [];
+  for (const key of data.items) {
+    dataToSend.push({
+      nextPageToken: data.nextPageToken,
+      videoId: key.id.videoId,
+      title: key.snippet.title,
+      description: key.snippet.description,
+      thumbnailLink: key.snippet.thumbnails.medium.url,
+    });
+  }
+  return dataToSend;
 }
