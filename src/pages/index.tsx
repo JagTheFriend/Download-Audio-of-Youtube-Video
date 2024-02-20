a486f75c8580715b9f75a0a286873129d3caea8c
JagTheFriend <JagTheFriend12@gmail.com>
2024-02-20 11:35:03 +0200

    Created url and click it once downloading has finished
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,10 +53,26 @@ function DisplayResults({ data }: { data: DataToSend[] }) {
 
   useEffect(() => {
     if (selectedVid.songId.length === 0) return;
-    axios.post("/api/yt", {
-      songId: selectedVid.songId,
-      fileName: selectedVid.fileName,
-    });
+    axios
+      .post("/api/yt", {
+        songId: selectedVid.songId,
+        fileName: selectedVid.fileName,
+      })
+      .then((response) => {
+        // create file link in browser's memory
+        const href = URL.createObjectURL(response.data);
+
+        // create "a" HTML element with href to file & click
+        const link = document.createElement("a");
+        link.href = href;
+        link.setAttribute("download", "file.pdf"); //or any other extension
+        document.body.appendChild(link);
+        link.click();
+
+        // clean up "a" element & remove ObjectURL
+        document.body.removeChild(link);
+        URL.revokeObjectURL(href);
+      });
   }, [selectedVid]);
 
   if (data.length === 0) return null;
