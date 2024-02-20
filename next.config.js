d46432d54515f29b6ec80b9b6544e621978f9d64
JagTheFriend <JagTheFriend12@gmail.com>
2024-02-20 10:02:44 +0200

    Updated config
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- next.config.js
+++ next.config.js
@@ -6,17 +6,27 @@ await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  reactStrictMode: true,
+	reactStrictMode: true,
 
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+	/**
+	 * If you are using `appDir` then you must comment the below `i18n` config out.
+	 *
+	 * @see https://github.com/vercel/next.js/issues/41980
+	 */
+	i18n: {
+		locales: ["en"],
+		defaultLocale: "en",
+	},
+
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "i.ytimg.com",
+				port: "",
+			},
+		],
+	},
 };
 
 export default config;
